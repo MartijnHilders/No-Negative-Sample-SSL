20a1c9a6269332e9a3a71125f8bb18ad3923721a
MartijnHilders <martijnhilders@live.nl>
2023-02-13 22:03:39 +0100

    changed config params

--- sweep.py
+++ sweep.py
@@ -1,6 +1,7 @@
 import wandb
 from utils.experiment_utils import load_yaml_to_dict
 
-config = load_yaml_to_dict("configs/sweeps/sweep_vicreg_inertial_skeleton_mmact.yaml")
+config = load_yaml_to_dict("configs/sweeps/sweep_cmc_cvkm_inertial_skeleton_mmact.yaml")
 sweep_id = wandb.sweep(config)
 wandb.agent(sweep_id)
+
